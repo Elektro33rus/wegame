3ce71aee059b1d7542163fe91cd7be2cb1192d0e
Mazion <mazion2014@yandex.ru>
2018-11-04 17:30:06 +0300

    2Movie

--- MyGAME1/android/src/ru/cursov/game/AndroidLauncher.java
+++ MyGAME1/android/src/ru/cursov/game/AndroidLauncher.java
@@ -1,16 +1,1 @@
-package ru.cursov.game;
-
-import android.os.Bundle;
-
-import com.badlogic.gdx.backends.android.AndroidApplication;
-import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
-import ru.cursov.game.Game;
-
-public class AndroidLauncher extends AndroidApplication {
-	@Override
-	protected void onCreate (Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
-		initialize(new Game(), config);
-	}
-}
+package ru.cursov.game;import android.os.Bundle;import com.badlogic.gdx.backends.android.AndroidApplication;import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;public class AndroidLauncher extends AndroidApplication {	@Override	protected void onCreate (Bundle savedInstanceState) {		super.onCreate(savedInstanceState);		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();		initialize(new MarioBros(), config);	}}
