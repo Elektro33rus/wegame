4d5e10d209f72c17c7342f427a7673a29e6a307e
Mazion <mazion2014@yandex.ru>
2018-11-05 15:15:47 +0300

    4Movie

--- MyGAME1/core/src/ru/cursov/game/Screens/PlayScreen.java
+++ MyGAME1/core/src/ru/cursov/game/Screens/PlayScreen.java
@@ -2,18 +2,29 @@ package ru.cursov.game.Screens;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.utils.viewport.FitViewport;
+import com.badlogic.gdx.utils.viewport.StretchViewport;
+import com.badlogic.gdx.utils.viewport.Viewport;
 
 import ru.cursov.game.MarioBros;
+import ru.cursov.game.Scenes.Hud;
 
 public class PlayScreen implements Screen {
     private MarioBros game;
     Texture texture;
+    private OrthographicCamera gamecam;
+    private Viewport gamePort;
+    private Hud hud;
 
     public PlayScreen(MarioBros game){
         this.game = game;
         texture = new Texture("badlogic.jpg");
+        gamecam = new OrthographicCamera();
+        gamePort = new FitViewport(MarioBros.V_WIDTH, MarioBros.V_HEIGHT, gamecam);
+        hud = new Hud(game.batch);
     }
 
     @Override
@@ -23,16 +34,16 @@ public class PlayScreen implements Screen {
 
     @Override
     public void render(float delta){
-        Gdx.gl.glClearColor(1, 0,0,1);
+        Gdx.gl.glClearColor(0, 0,0,1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-        game.batch.begin();
-        game.batch.draw(texture,0,0);
-        game.batch.end();
+
+        game.batch.setProjectionMatrix(hud.stage.getCamera().combined);
+        hud.stage.draw();
     }
 
     @Override
     public void resize(int width, int height){
-
+        gamePort.update(width,height);
     }
 
     @Override
